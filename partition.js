c4887e4975322274706b7e499d524177674cfdf7
aprajitashh-ctrl <aprajitashh@gmail.com>
2026-09-13 19:42:19 +0530

    Add files via upload

--- partition.js
+++ partition.js
@@ -1,7 +1,8 @@
 import { not } from '../util/not';
-import { filter } from '../operators/filter';
-import { innerFrom } from './innerFrom';
-export function partition(source, predicate, thisArg) {
-    return [filter(predicate, thisArg)(innerFrom(source)), filter(not(predicate, thisArg))(innerFrom(source))];
+import { filter } from './filter';
+export function partition(predicate, thisArg) {
+    return function (source) {
+        return [filter(predicate, thisArg)(source), filter(not(predicate, thisArg))(source)];
+    };
 }
 //# sourceMappingURL=partition.js.map
